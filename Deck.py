a3f81afd5e0a1a098ce1a586b7d070eae55c13fa
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-09-25 19:25:47 +0530

    Complete game logic

--- Deck.py
+++ Deck.py
@@ -4,9 +4,9 @@ def createDeck():
     Deck = list()
 
     #Set a variable for faceValues
-    cards = ['🅰️','2','3','4','5','6','7','8','9','10' '🇯', '🤴', '🤴']
+    cards = ['A','2','3','4','5','6','7','8','9','10', 'J', 'Q', 'K']
     for card in cards: # Adding numbers 
-        for ctype in ['♠','♣️','♥️','♦️']:  # There are 4 different suites
+        for ctype in ['C','S','H','D']:  # There are 4 different suites
             Deck.append(card+"  "+ctype)
 
     shuffle(Deck) # Mixing results with shuffle
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from Deck import createDeck
-
+from random import shuffle
 
 def manual():
     print(f"Beggar My Neighbor, a card game suitable for small children that’s also known as Beat Your Neighbor Out of Doors and\nStrip Jack Naked, is a very simple game, requiring no strategy or planning at all. \nThe objective of the game is to win all the cards from the other players.")
@@ -18,14 +18,40 @@ class Game:
     def __init__(self, player1,player2):
         self.player1 = player1
         self.player2 = player2
-
+        self.stk=[]
+        self.chance=True
     def playOne(self):
-        top = player1.cards.pop()
-        return top
+        if len(player1.cards)!=0:
+            top = player1.cards.pop()
+            self.stk.append(top)
+            self.chance =True
+            shuffle(player1.cards)
+            return top
+        return "_"
     def playTwo(self):
-        top = player2.cards.pop()
-        return top
-
+        if len(player2.cards)!=0:
+            top = player2.cards.pop()
+            self.stk.append(top)
+            self.chance=False
+            shuffle(player2.cards)
+            return top
+        return "_"
+    
+    def winning_stk(self):
+        if len(self.stk)>=2:
+            c1 = self.stk[-1]
+            c2 =self.stk[-2]
+            if c1[-1]==c2[-1]:
+                print("matching "+c1[-1])
+                print("matching "+c2[-1])
+                if self.chance==True:
+                    player1.cards.extend(self.stk)
+                    print(player1.name+" got all the cards")
+                else:
+                    player2.cards.extend(self.stk)
+                    print(player2.name+" got all the cards")
+                self.stk=[]
+            
 
 # Create card Deck
 cardDeck = createDeck()
@@ -51,18 +77,29 @@ chance=True
 game= Game(player1,player2)
 while a != '--quit':
     a = input()
-    if a == "--help":
+    if a == "--help": # Asking for manual
         manual()
-        a=input()
+        a=input()  # getting the input as --resume
     if a=="--resume":
         pass
-    if chance==True:
-        print(player1.name+" chance")
-        print(game.playOne())
-    else:
-        print(player2.name+" chance")
-        print(game.playTwo())
-    
+    if chance==True: # player 1 chance
+        print(player1.name+" chance "+str(len(player1.cards)) +" card/s left")
+        card = game.playOne() 
+        if card=="_":
+            print(player2.name+" wins the game ")
+            break
+        print(card)
+    else: # Player 2 Chance
+        print(player2.name+" chance"+str(len(player2.cards)) +" card/s left")
+        card =game.playTwo()
+        if card=="_":
+            print(player1.name+" wins the game ")
+            break
+        print(card)
+    print("Cards on the table :- "+str(len(game.stk)))
+        
+    game.winning_stk()
     chance=(~chance)
+    
     
     
